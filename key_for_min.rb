21988c08a4862e07c61924894adcbc5464bf9ce0
Jason Leach <jlspursfan@gmail.com>
2019-05-17 21:34:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_val = 1000
+  min_key = 0
+  
   name_hash.each do |k, v|
-    min_val = 1000
-    min_key = 0
     if v < min_val
       v = min_val
       k = min_key
     end
   end
+  
   return min_key
 end
