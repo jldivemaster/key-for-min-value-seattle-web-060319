214630cbf97a59f7aad0c248d267f919db996433
Jason Leach <jlspursfan@gmail.com>
2019-05-17 21:34:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,13 @@
 def key_for_min_value(name_hash)
   min_val = 1000
   min_key = 0
-  
+
   name_hash.each do |k, v|
     if v < min_val
       v = min_val
       k = min_key
     end
   end
-  
+
   return min_key
 end
